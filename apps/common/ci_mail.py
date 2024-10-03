8abab5b4b4b3ea5ceeac082fc0a32f5b80fdbb71
jasurbekcodes <jasuribodullayev0103@gmail.com>
2024-10-03 20:16:28 +0500

    mail

---
+++ apps/common/ci_mail.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+
+from django.core.mail import send_mail
+
+
+class Command(BaseCommand):
+    help = 'Send CI notification'
+
+    def handle(self, *args, **options):
+        send_mail('Test mail', 'this is a test mail',
+                  'jasuribodullayev0103@gmail.com', ['jasuribodullayev0103@gmail.com'],
+                  fail_silently=False)
+        self.stdout.write(self.style.SUCCESS("Email jo'natildi"))
